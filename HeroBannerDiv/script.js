75ebbfcb549e8cf79ecfebe03a238001e6736b10
Szulc Konrad <T0254779@gemalto.com>
2023-03-24 14:18:22 +0100

    Added transition handling - no reaction to button during transition

--- HeroBannerDiv/script.js
+++ HeroBannerDiv/script.js
@@ -3,59 +3,64 @@ kszulc 21.03.2023
 JS, HTML manipulation excercise
 */
 
-window.onload = function (){
-    start();
-}
+window.onload = function () {
+  start();
+};
 
 var mainDiv = document.querySelector(".hero-banner-container");
+var stateBlocked = false;
 
-function start (){
-
-    // Link buttons
-    let startButtonElement = document.getElementById("startButton");
-    let resetBtnElement = document.getElementById("resetButton");
-    // Add Action(Event) listeners to buttons
-    startButtonElement.addEventListener("click", loadDiv);
-    resetBtnElement.addEventListener("click", resetMainDiv);
-
+function start() {
+  // Link buttons
+  let startButtonElement = document.getElementById("startButton");
+  let resetBtnElement = document.getElementById("resetButton");
+  // Add Action(Event) listeners to buttons
+  startButtonElement.addEventListener("click", loadDiv);
+  resetBtnElement.addEventListener("click", resetMainDiv);
 }
 
 function loadDiv() {
-
+  if (!stateBlocked) {
     resetMainDiv(); //Applied optionally to reset/remove div list after button was pressed before and avoid duplicates and main div boundry cross (no overflow setting applied in css)
+    stateBlocked = true;
+    mainDiv.setAttribute("class", "hero-banner-container animated-in"); // 'setAttribute' (unlike classList.add) resets/removes existing classes in element and applies defined ones in the command
 
-    //  mainDiv.classList.add("animated-in");  // 'classList.add' doesn't reset element's "class" attribute, just adds defined ones in the command to existing element's classes
-
-    mainDiv.setAttribute("class", "hero-banner-container animated-in"); // 'setAttribute' resets/removes existing classes in element and applies defined ones in the command
-
-    for(let i=0; i< 10; i++){
-        let childDiv = document.createElement("div");
-        childDiv.textContent = "Item " + (i+1);
-        mainDiv.appendChild(childDiv);
-        if(i==9){
-            setTimeout(function () {
-                mainDiv.classList.remove("animated-in");
-                mainDiv.classList.add("animated-out");
-                // mainDiv.setAttribute("class", "animated-out");
-                
-            }, 300)
-            
-        }
-        
+    for (let i = 0; i < 10; i++) {
+      let childDiv = document.createElement("div");
+      childDiv.textContent = "Item " + (i + 1);
+      mainDiv.appendChild(childDiv);
+      if (i == 9) {
+        transitionsHandler();
+      }
     }
+  }
+}
 
+function transitionsHandler() {
+  let transitionEl = document.querySelector(".animated-in");
+  transitionEl.addEventListener("transitionend", function (e) {
+    //First transition handler -> animated-in
+    mainDiv.classList.add("animated-out");
+    stateBlocked = true; //Keep state blocked to avoid Start button reaction during transition time
+    transitionEl.addEventListener("transitionend", function (e) {
+      //Second transition handler -> animated-out
+      if (e.target.matches(".animated-out")) {
+        stateBlocked = false; // Unblock state to allow entire loadDiv function to be run again
+        return;
+      }
+    });
+    return; //Returns added just in case - 'transitionend' inspects all transitions and can run commands multiple times, need to find more efficient solution
+  });
 }
 
 function resetMainDiv() {
-    
-    if(mainDiv.classList.value !== "hero-banner-container"){  // Reset main div class only when reset button is pressed
-        mainDiv.setAttribute("class", "hero-banner-container");
-    }
-        var lastElement = mainDiv.lastElementChild;     //Remove child divs section
-        while(lastElement) {
-        mainDiv.removeChild(lastElement);
-        lastElement = mainDiv.lastElementChild;
-    }
-
+  if (mainDiv.classList.value !== "hero-banner-container") {
+    // Reset main div class only when reset button is pressed
+    mainDiv.setAttribute("class", "hero-banner-container");
+  }
+  var lastElement = mainDiv.lastElementChild; //Remove child divs section
+  while (lastElement) {
+    mainDiv.removeChild(lastElement);
+    lastElement = mainDiv.lastElementChild;
+  }
 }
-
--- HeroBannerDiv/script2.js
+++ HeroBannerDiv/script2.js
@@ -3,61 +3,66 @@ kszulc 21.03.2023
 JS, HTML manipulation excercise
 */
 
-window.onload = function (){
-    start();
-}
+window.onload = function () {
+  start();
+};
 
-const itemNb = [1,2,3,4,5,6,7,8,9,10];
+const itemNb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var mainDiv = document.querySelector(".hero-banner-container");
+var stateBlocked = false;
 
-function start (){
-
-    // Link buttons
-    let startButtonElement = document.getElementById("startButton");
-    let resetBtnElement = document.getElementById("resetButton");
-    // Add Action(Event) listeners to buttons
-    startButtonElement.addEventListener("click", loadDiv);
-    resetBtnElement.addEventListener("click", resetMainDiv);
-
+function start() {
+  // Link buttons
+  let startButtonElement = document.getElementById("startButton");
+  let resetBtnElement = document.getElementById("resetButton");
+  // Add Action(Event) listeners to buttons
+  startButtonElement.addEventListener("click", loadDiv);
+  resetBtnElement.addEventListener("click", resetMainDiv);
 }
 
 function loadDiv() {
+  if (!stateBlocked) {
+    resetMainDiv(); //Applied optionally to reset/remove div list after button was pressed before and avoid duplicates and main div boundry cross (no overflow setting applied in css)
+    stateBlocked = true;
+    mainDiv.setAttribute("class", "hero-banner-container animated-in"); // 'setAttribute' (unlike classList.add) resets/removes existing classes in element and applies defined ones in the command
 
-    resetMainDiv(); //Applied optionally to reset div list and avoid duplicates and main div boundry cross (no overflow setting applied in css)
+    itemNb.forEach((number) => {
+      let childDiv = document.createElement("div");
+      childDiv.textContent = "Item " + number;
+      mainDiv.appendChild(childDiv);
 
-    //  mainDiv.classList.add("animated-in");  // 'classList.add' doesn't reset element's "class" attribute, just adds defined ones in the command to existing element's classes
-
-    mainDiv.setAttribute("class", "hero-banner-container animated-in"); // 'setAttribute' resets/removes existing classes in element and applies defined ones in the command
-
-    itemNb.forEach(number => {
-        let childDiv = document.createElement("div");
-        childDiv.textContent = "Item " + number;
-        mainDiv.appendChild(childDiv);
-
-        if(number==itemNb[itemNb.length-1]){
-            setTimeout(function () {
-                mainDiv.classList.remove("animated-in");
-                mainDiv.classList.add("animated-out");
-                // mainDiv.setAttribute("class", "animated-out");
-                mainDiv.setAttribute("class", "hero-banner-container animated-out"); //Example of how to add two classes in setAttribute               
-            }, 300)
-        }       
-    })
-
+      if (number == itemNb[itemNb.length - 1]) {
+        transitionsHandler();
+      }
+    });
+  }
 }
 
-
-
+function transitionsHandler() {
+  let transitionEl = document.querySelector(".animated-in");
+  transitionEl.addEventListener("transitionend", function (e) {
+    //First transition handler -> animated-in
+    mainDiv.classList.add("animated-out");
+    stateBlocked = true; //Keep state blocked to avoid Start button reaction during transition time
+    transitionEl.addEventListener("transitionend", function (e) {
+      //Second transition handler -> animated-out
+      if (e.target.matches(".animated-out")) {
+        stateBlocked = false; // Unblock state to allow entire loadDiv function to be run again
+        return;
+      }
+    });
+    return; //Returns added just in case - 'transitionend' inspects all transitions and can run multiple commands, need to find more efficient solution
+  });
+}
 
 function resetMainDiv() {
-    
-    if(mainDiv.classList.value !== "hero-banner-container"){  // Reset main div class only when reset button is pressed
-        mainDiv.setAttribute("class", "hero-banner-container");
-    }
-        var lastElement = mainDiv.lastElementChild;     //Remove child divs section
-        while(lastElement) {
-        mainDiv.removeChild(lastElement);
-        lastElement = mainDiv.lastElementChild;
-    }
-
+  if (mainDiv.classList.value !== "hero-banner-container") {
+    // Reset main div class only when reset button is pressed
+    mainDiv.setAttribute("class", "hero-banner-container");
+  }
+  var lastElement = mainDiv.lastElementChild; //Remove child divs section
+  while (lastElement) {
+    mainDiv.removeChild(lastElement);
+    lastElement = mainDiv.lastElementChild;
+  }
 }
