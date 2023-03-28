f166dbf6e2e0e4e9386b2154dc242a67749f73cf
Szulc Konrad <T0254779@gemalto.com>
2023-03-28 21:38:50 +0200

    Added delay between items adding, changed method of transitions handling, other minor changes

--- HeroBannerDiv/script.js
+++ HeroBannerDiv/script.js
@@ -3,12 +3,19 @@ kszulc 21.03.2023
 JS, HTML manipulation excercise
 */
 
+// Init values and variables
+const wait = (msec) => new Promise((resolve, _noRejectParam) => {
+    setTimeout(resolve, msec);
+  });
+const itemDelay = 500; // Delay time (in ms) between items adding
+var animatedOutDur = ""; //Stores transition duration from css for animted-out class - value taken when class loaded to div
+var abort = ""; //Flag for reset button to abort async function - without it items keep adding until all are added, even after the reset is pressed
+var mainDiv = document.querySelector(".hero-banner-container");
+var stateUnblocked = true;
+
 window.onload = function () {
   start();
 };
-
-var mainDiv = document.querySelector(".hero-banner-container");
-var stateUnblocked = true;
 
 function start() {
   // Link buttons
@@ -24,34 +31,31 @@ function loadDiv() {
     return;
   }
   resetMainDiv(); //Applied optionally to reset/remove div list after button was pressed before and avoid duplicates and main div boundry cross (no overflow setting applied in css)
-  stateUnblocked = true;
   mainDiv.setAttribute("class", "hero-banner-container animated-in"); // 'setAttribute' (unlike classList.add) resets/removes existing classes in element and applies defined ones in the command
+  mainDivHandler();
+}
 
+async function mainDivHandler() {
+  abort = false;
+  stateUnblocked = false;
   for (let i = 0; i < 10; i++) {
+    if (abort) {
+      //Checks if loop needs to be aborted - applicable when Reset button is pressed
+      return;
+    }
     let childDiv = document.createElement("div");
     childDiv.textContent = "Item " + (i + 1);
     mainDiv.appendChild(childDiv);
+    await wait(itemDelay); //Delay between item adding
     if (i == 9) {
-      transitionsHandler();
+      mainDiv.classList.add("animated-out");
+      animatedOutDur = getTransitionDurationFromCSS(
+        document.querySelector(".animated-out")
+      );
+      await wait(animatedOutDur); //Wait for transition/animation to finish in order to unblock state
+      stateUnblocked = true;
     }
   }
-}
-
-function transitionsHandler() {
-  let transitionEl = document.querySelector(".animated-in");
-  transitionEl.addEventListener("transitionend", function (e) {
-    //First transition handler -> animated-in
-    mainDiv.classList.add("animated-out");
-    stateUnblocked = false; //Keep state blocked to avoid Start button reaction during transition time
-    transitionEl.addEventListener("transitionend", function (e) {
-      //Second transition handler -> animated-out
-      if (e.target.matches(".animated-out")) {
-        stateUnblocked = true; // Unblock state to allow entire loadDiv function to be run again
-        return;
-      }
-    });
-    return; //Returns added just in case - 'transitionend' inspects all transitions and can run commands multiple times, need to find more efficient solution
-  });
 }
 
 function resetMainDiv() {
@@ -63,5 +67,11 @@ function resetMainDiv() {
   while (lastElement) {
     mainDiv.removeChild(lastElement);
     lastElement = mainDiv.lastElementChild;
+    abort = true; //Set abort flag to true - aborts loop in loadDiv function
+    stateUnblocked = true; //Unblocks the flag after reset
   }
+}
+
+function getTransitionDurationFromCSS(element) {
+  return parseFloat(this.getComputedStyle(element).transitionDuration) * 1000;
 }
